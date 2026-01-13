ce699685070b7ca692bdd853a2d95e0a5dcb7086
Lucas de Souza Gomes <programmingpyrubygo@gmail.com>
2026-01-13 11:09:08 -0300

    feat: updating csv example and improving data_quality

---
+++ src/data_quality.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def check_missing_values(df):
+    total = df.shape[0]
+    missing = df.isnull().sum()
+    percent = (missing / total) * 100
+
+    return pd.DataFrame({
+        "faltantes": missing,
+        "percentual (%)": percent.round(2)
+    })
+
+
+def detect_outliers_iqr(series):
+    q1 = series.quantile(0.25)
+    q3 = series.quantile(0.75)
+    iqr = q3 - q1
+
+    lower = q1 - 1.5 * iqr
+    upper = q3 + 1.5 * iqr
+
+    outliers = series[(series < lower) | (series > upper)]
+    return outliers
